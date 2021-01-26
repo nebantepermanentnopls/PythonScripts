472dafbe4d276001e683f876d91ed4f0aa070e22
nebantepermanentnopls <secretlelik@gmail.com>
2021-01-26 12:42:49 +0300

    add litle fixes

--- clear_logfile.py
+++ clear_logfile.py
@@ -1,19 +1,19 @@
-import sys
-import os
-import shutil
+from sys import argv
+from os import stat, path
+from shutil import copyfile
 
 # clear_logfile file.log 1 5
 
-if len(sys.argv) != 4:
+if len(argv) != 4:
     print('Use 4 args for script: "clear_logfile.py file.log 1 5"')
     exit(1)
 
-file_name = sys.argv[1]
-max_size = int(sys.argv[2])
-logs_number = int(sys.argv[3])
+file_name = argv[1]
+max_size = int(argv[2])
+logs_number = int(argv[3])
 
-if os.path.isfile(file_name):
-    logfile_size = os.stat(file_name).st_size / 1024
+if path.isfile(file_name):
+    logfile_size = stat(file_name).st_size / 1024
 
     if logfile_size >= max_size:
 
@@ -23,12 +23,13 @@ if os.path.isfile(file_name):
                 src = file_name + '_' + str(i-1)
                 dst = file_name + '_' + str(i)
 
-                if os.path.isfile(src):
-                    shutil.copyfile(src, dst)
+                if path.isfile(src):
+                    copyfile(src, dst)
                     print('Copying:' + src + ' -> ' + dst)
 
-            shutil.copyfile(file_name, file_name + '_1')
+            copyfile(file_name, file_name + '_1')
             print('Copying:' + file_name + '   -> ' + file_name + '_1')
+
         with open(file_name, 'w'):
             print('Cleaning:' + file_name)
         print('Done')
