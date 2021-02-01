88e7b32180980a26c35b0cd5a2a8a58545c6336f
nebantepermanentnopls <secretlelik@gmail.com>
2021-02-01 18:12:06 +0300

    add src for prime numbers

---
+++ simple.py
@@ -0,0 +1,20 @@
+# 2 3 5 7 ..
+# самый простой алг
+# for i in range(1, 200000):
+#    k = 0
+#    for j in range(2, i-1):
+#        if i % j == 0:
+#            k += 1
+#    if k == 0:
+#        print(i)
+
+simple = []
+for i in range(2, 200000):
+    for j in simple:
+        if i % j == 0:
+            break
+    else:
+        simple.append(i)
+
+print(simple)
+
