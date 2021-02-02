c01a5d82adf01416577faccbd98608eb5c5f0868
nebantepermanentnopls <64306599+nebantepermanentnopls@users.noreply.github.com>
2021-02-02 20:50:10 +0300

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 print(2)
 print(3)
 for i in range(3, 110000, 2):
-    if i < 10 or i % 10 != 5:
+    if i % 10 != 5 or i < 10:
         for j in range(3, i, 2):
             if j * j > i:
                 print(i)
